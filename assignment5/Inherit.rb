f7187fa456090d2c15cb8d30d3cedef12eec7aef
namithachandran <37650723+namithachandran@users.noreply.github.com>
2019-02-06 14:50:02 +0530

    Add files via upload

---
+++ assignment5/Inherit.rb
@@ -0,0 +1,22 @@
+    class Animal 
+      def animal
+        puts "Dog is a Animal"  
+      end  
+      def species
+        puts "Dog is a sub species of wolf" 
+    end  
+  end
+
+      
+    class Dog < Animal  
+      def sound
+        puts "it sounds like arff"  
+      end  
+    end  
+      
+    obj = Dog.new  
+    obj.animal
+    obj.species  
+    obj.sound
+  
+
---
+++ assignment5/override.rb
@@ -0,0 +1,22 @@
+    class Animal 
+      def animal
+        puts "Dog is a Animal"  
+      end  
+      def species
+        puts "Dog is a sub species of wolf" 
+    end  
+  end
+
+      
+    class Dog < Animal  
+      def species
+        puts "Dog sounds like arff"  
+      end  
+    end  
+      
+    obj = Dog.new  
+    obj.animal
+    obj.species  
+    
+  
+
